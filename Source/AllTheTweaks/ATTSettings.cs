ee016a9ae7a2a55fb9b4fbb06fc2b6b7ceea4dc5
Robmart <robinmartensson01@gmail.com>
2021-07-08 04:58:37 +0200

    Settings class

---
+++ Source/AllTheTweaks/ATTSettings.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+
+namespace AllTheTweaks {
+	public class ATTSettings : ModSettings {
+		public bool useThrumboMilk;
+		public bool useThrumboCheese;
+		public bool useThrumboWool;
+		public bool isAmbrosiaGrowable;
+
+		public override void ExposeData()
+		{
+			Scribe_Values.Look(ref useThrumboMilk, "useThrumboMilk", true);
+			Scribe_Values.Look(ref useThrumboCheese, "useThrumboCheese", true);
+			Scribe_Values.Look(ref useThrumboWool, "useThrumboWool", true);
+			Scribe_Values.Look(ref isAmbrosiaGrowable, "isAmbrosiaGrowable", true);
+			base.ExposeData();
+		}
+	}
+}
